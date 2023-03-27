13bfd1e18dcc9e4afd7697db4aa44a2996e1008b
annushka <anna.bondarenko1@nure.ua>
2023-03-27 22:02:33 +0200

    Add project files.

---
+++ Domain/Base/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Base
+{
+    public abstract class BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+    }
+}
