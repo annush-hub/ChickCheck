82de40965135a5d877f149c6fd55b75c2424d68a
annushka <anna.bondarenko1@nure.ua>
2023-03-27 22:51:53 +0200

    Barn class added

---
+++ Domain/Barn.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Barn : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float TemperatureInCelsius { get; set; }
+        public float TemperatureInFahrenheit { get; set; }      
+
+        public Guid ProductId { get; set; }
+    }
+}
