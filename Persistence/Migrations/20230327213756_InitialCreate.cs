0f0a7a980b048e571b0069599c411845b8e21e9f
annushka <anna.bondarenko1@nure.ua>
2023-03-27 23:40:34 +0200

    Entity Framework code first migratiton InitialCreate done

---
+++ Persistence/Migrations/20230327213756_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Barns",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TemperatureInCelsius = table.Column<float>(type: "real", nullable: false),
+                    TemperatureInFahrenheit = table.Column<float>(type: "real", nullable: false),
+                    IsDeactivated = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Barns", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Barns");
+        }
+    }
+}
