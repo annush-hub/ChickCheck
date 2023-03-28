c9355ea22fd1a06e80a5108682e0b3cddfd33963
annushka <anna.bondarenko1@nure.ua>
2023-03-28 09:18:47 +0200

    API controller for Barns added

---
+++ API/Controllers/BarnsController.cs
@@ -0,0 +1,31 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BarnsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public BarnsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Barn>>> GetBarns() 
+        {
+            return await _context.Barns.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Barn>> GetBarn(Guid id)
+        {
+            return await _context.Barns.FindAsync(id);
+        }
+    }
+}
