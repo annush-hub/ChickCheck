58cd3051a3bf4606de2db34be96d3e320a0ce71d
annushka <anna.bondarenko1@nure.ua>
2023-03-30 18:57:43 +0200

    FeederDto added

---
+++ Application/Feeders/FeederDto.cs
@@ -0,0 +1,18 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Feeders
+{
+    public class FeederDto
+    {
+        public Guid Id { get; set; }
+        public float Capacity { get; set; }
+        public float Fullness { get; set; }
+        public bool IsInUse { get; set; }
+        public Guid BarnId { get; set; }
+    }
+}
