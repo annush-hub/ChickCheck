3069c03d95dd1815e51b2844e03a075829ce5b6a
annushka <anna.bondarenko1@nure.ua>
2023-03-30 09:19:25 +0200

    MappingProfiles updated: EggGrade -> EggGradeDto map added

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -1,5 +1,5 @@
-﻿
-using Application.Barns;
+﻿using Application.Barns;
+using Application.EggGrades;
 using AutoMapper;
 using Domain;
 using System.Diagnostics;
@@ -15,6 +15,11 @@ namespace Application.Core
             CreateMap<Barn, BarnDto>()
                 .ForMember(d => d.EggGradeEU, o => o.MapFrom(src => src.EggGrade.GradeEU))
                 .ForMember(d => d.EggGradeUA, o => o.MapFrom(src => src.EggGrade.GradeUA));
+
+            CreateMap<EggGrade, EggGradeDto>()
+                .ForMember(d => d.Barns,
+                o => o.MapFrom(src => src.Barns.ToList()));
+              
         }
     }
 }
