f5502946af0106e9d0a772d9d761735f38a34548
annushka <anna.bondarenko1@nure.ua>
2023-03-30 10:59:11 +0200

    Feeder added

---
+++ Domain/Feeder.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Base;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain
+{
+    public class Feeder : BaseEntity
+    {
+        public float Capacity { get; set; }
+        public float Fullness { get; set; }
+        public bool IsInUse { get; set; }
+        public Guid BarnId { get; set; }
+        public Barn Barn { get; set; }
+    }
+}
---
+++ Persistence/Migrations/20230330075253_FeederAdded.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class FeederAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Feeders",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Capacity = table.Column<float>(type: "real", nullable: false),
+                    Fullness = table.Column<float>(type: "real", nullable: false),
+                    IsInUse = table.Column<bool>(type: "bit", nullable: false),
+                    BarnId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Feeders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Feeders_Barns_BarnId",
+                        column: x => x.BarnId,
+                        principalTable: "Barns",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Feeders_BarnId",
+                table: "Feeders",
+                column: "BarnId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Feeders");
+        }
+    }
+}
