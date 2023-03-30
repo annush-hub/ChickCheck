5ad7195fcefc93a0e5c21a04a9cc86bce72c5ba9
annushka <anna.bondarenko1@nure.ua>
2023-03-30 08:41:30 +0200

    BarnDto created

---
+++ Application/Barns/BarnDto.cs
@@ -0,0 +1,21 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Barns
+{
+    public class BarnDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float TemperatureInCelsius { get; set; }
+        public float TemperatureInFahrenheit { get; set; }
+        public bool IsDeactivated { get; set; }
+        public string EggGradeUA { get; set; }
+        public string EggGradeEU { get; set; }
+    }
+}
