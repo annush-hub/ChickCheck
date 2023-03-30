226af3faf432d8e8681b08fd6e0af81908917f34
annushka <anna.bondarenko1@nure.ua>
2023-03-30 09:20:23 +0200

    EggGradesController updated

--- API/Controllers/EggGradesController.cs
+++ API/Controllers/EggGradesController.cs
@@ -11,13 +11,13 @@ namespace API.Controllers
     public class EggGradesController : BaseApiController
     {
         [HttpGet]
-        public async Task<ActionResult<List<EggGrade>>> GetEggGrades()
+        public async Task<ActionResult<List<EggGradeDto>>> GetEggGrades()
         {
             return await Mediator.Send(new EggGradeList.Query());
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<EggGrade>> GetEggGrade(Guid id)
+        public async Task<ActionResult<EggGradeDto>> GetEggGrade(Guid id)
         {
             return await Mediator.Send(new EggGradeDetails.Query { Id = id });
         }
