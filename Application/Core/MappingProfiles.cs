61a2f7016956c8d7b5c007df03a69635e69ec183
annushka <anna.bondarenko1@nure.ua>
2023-03-30 08:42:03 +0200

    MappingProfiles updated: Map Barn -> BarnDto added

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -1,4 +1,5 @@
 ﻿
+using Application.Barns;
 using AutoMapper;
 using Domain;
 using System.Diagnostics;
@@ -10,6 +11,10 @@ namespace Application.Core
         public MappingProfiles()
         {
             CreateMap<Barn, Barn>();
+
+            CreateMap<Barn, BarnDto>()
+                .ForMember(d => d.EggGradeEU, o => o.MapFrom(src => src.EggGrade.GradeEU))
+                .ForMember(d => d.EggGradeUA, o => o.MapFrom(src => src.EggGrade.GradeUA));
         }
     }
 }
