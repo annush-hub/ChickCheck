8550d5879b805443c223af56cd64e4a9417c6abb
annushka <anna.bondarenko1@nure.ua>
2023-03-30 08:43:19 +0200

    BarnList refactoring done

--- Application/Barns/BarnList.cs
+++ Application/Barns/BarnList.cs
@@ -1,14 +1,8 @@
 ﻿using AutoMapper;
 using AutoMapper.QueryableExtensions;
-using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.Barns
 {
@@ -34,7 +28,6 @@ namespace Application.Barns
                     .ToListAsync(cancellationToken: cancellationToken);
 
                 return barns;
-                //return await _context.Barns.ToListAsync();
             }
         }
     }
