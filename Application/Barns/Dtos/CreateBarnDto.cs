90978692caf48ac65ade156bb3b9cab58c623884
annushka <anna.bondarenko1@nure.ua>
2023-03-30 19:24:16 +0200

    UpdateBarnDto deleted, CreateBarnDro updated

--- Application/Barns/Dtos/CreateBarnDto.cs
+++ Application/Barns/Dtos/CreateBarnDto.cs
@@ -9,6 +9,7 @@ namespace Application.Barns.Dtos
 {
     public class CreateBarnDto
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public float TemperatureInCelsius { get; set; }
