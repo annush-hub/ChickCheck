5955bf7f53ddf76603c84cc99ecd5b4d9c8aae39
annushka <anna.bondarenko1@nure.ua>
2023-03-30 18:58:47 +0200

    FeederList handler added

---
+++ Application/Feeders/FeederList.cs
@@ -0,0 +1,40 @@
+﻿using Application.EggGrades;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Feeders
+{
+    public class FeederList
+    {
+        public class Query : IRequest<List<FeederDto>> { }
+
+        public class Handler : IRequestHandler<Query, List<FeederDto>>
+        {
+            private readonly AppDbContext _context;
+            private readonly IMapper _mapper;
+
+            public Handler(AppDbContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+
+            public async Task<List<FeederDto>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var feeder = await _context.Feeders
+                    .ProjectTo<FeederDto>(_mapper.ConfigurationProvider)
+                    .ToListAsync(cancellationToken: cancellationToken);
+
+                return feeder;
+            }
+        }
+    }
+}
