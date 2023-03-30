6c546c198f0d7b21828dc6c838f940bac32b8a21
annushka <anna.bondarenko1@nure.ua>
2023-03-30 18:18:19 +0200

    CreateFeeder handler added

---
+++ API/Controllers/FeedersController.cs
@@ -0,0 +1,21 @@
+﻿using Application.Barns.Dtos;
+using Application.Barns;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Application.Feeders;
+using Domain;
+using Application.Feeders.Dtos;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeedersController : BaseApiController
+    {
+        [HttpPost]
+        public async Task<IActionResult> CreateFeeder([FromBody] FeederDto feeder)
+        {
+            return Ok(await Mediator.Send(new CreateFeeder.Command { Feeder = feeder }));
+        }
+    }
+}
---
+++ Application/Feeders/CreateFeeder.cs
@@ -0,0 +1,49 @@
+﻿using Application.Barns.Dtos;
+using Application.Feeders.Dtos;
+using Domain;
+using MediatR;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Feeders
+{
+    public class CreateFeeder
+    {
+        public class Command : IRequest
+        {
+            public FeederDto Feeder { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly AppDbContext _context;
+
+            public Handler(AppDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var feeder = new Feeder
+                {
+                    Capacity = request.Feeder.Capacity,
+                    Fullness = request.Feeder.Fullness,
+                    IsInUse = true,
+                    BarnId = request.Feeder.BarnId,
+                };
+
+
+                _context.Feeders.Add(feeder);
+
+                await _context.SaveChangesAsync();
+
+                return Unit.Value;
+            }
+        }
+    }
+}
