eee38d1ef8a1ddb2a496b8337a3dc27c646fa4cc
annushka <anna.bondarenko1@nure.ua>
2023-03-30 19:36:48 +0200

    BarnShortDto added

---
+++ Application/Barns/Dtos/BarnShortDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Barns.Dtos
+{
+    public class BarnShortDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float TemperatureInCelsius { get; set; }
+        public float TemperatureInFahrenheit { get; set; }
+        public bool IsDeactivated { get; set; }
+    }
+}
