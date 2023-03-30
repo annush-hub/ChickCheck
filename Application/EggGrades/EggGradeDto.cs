dc2425c206d0dc75fb12d3cb93886e96fc4dbb2d
annushka <anna.bondarenko1@nure.ua>
2023-03-30 09:18:46 +0200

    EggGradeDto added

---
+++ Application/EggGrades/EggGradeDto.cs
@@ -0,0 +1,19 @@
+﻿using Application.Barns;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.EggGrades
+{
+    public class EggGradeDto
+    {
+        public Guid Id { get; set; }
+        public string GradeUA { get; set; }
+        public string GradeEU { get; set; }
+
+        public ICollection<BarnDto> Barns { get; set; }
+    }
+}
