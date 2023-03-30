32b922d95083b04b4df3c69f72b9021d274b4636
annushka <anna.bondarenko1@nure.ua>
2023-03-30 09:19:48 +0200

    EggGradeDetails handler updated

--- Application/EggGrades/EggGradeDetails.cs
+++ Application/EggGrades/EggGradeDetails.cs
@@ -1,33 +1,35 @@
-﻿using Domain;
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.EggGrades
 {
     public class EggGradeDetails
     {
-        public class Query : IRequest<EggGrade>
+        public class Query : IRequest<EggGradeDto>
         {
             public Guid Id { get; set; }
         }
 
-        public class Handler : IRequestHandler<Query, EggGrade>
+        public class Handler : IRequestHandler<Query, EggGradeDto>
         {
             private readonly AppDbContext _context;
+            private readonly IMapper _mapper;
 
-            public Handler(AppDbContext context)
+            public Handler(AppDbContext context, IMapper mapper)
             {
                 _context = context;
+                _mapper = mapper;
             }
 
-            async Task<EggGrade> IRequestHandler<Query, EggGrade>.Handle(Query request, CancellationToken cancellationToken)
+            async Task<EggGradeDto> IRequestHandler<Query, EggGradeDto>.Handle(Query request, CancellationToken cancellationToken)
             {
-                return await _context.EggGrades.FindAsync(request.Id);
+                var eggGrade = await _context.EggGrades
+                    .ProjectTo<EggGradeDto>(_mapper.ConfigurationProvider)
+                    .FirstOrDefaultAsync<EggGradeDto>(x => x.Id == request.Id);
+                return eggGrade;
             }
         }
     }
