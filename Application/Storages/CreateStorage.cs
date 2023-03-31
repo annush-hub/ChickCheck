e4c0d678a065fb265bb57f86c02b6383897365bb
annushka <anna.bondarenko1@nure.ua>
2023-03-31 09:52:22 +0200

    CreateStorage handler added

---
+++ Application/Storages/CreateStorage.cs
@@ -0,0 +1,48 @@
+﻿using Application.Feeders;
+using Domain;
+using MediatR;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Storages
+{
+    public class CreateStorage
+    {
+        public class Command : IRequest
+        {
+            public StorageDto Storage { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly AppDbContext _context;
+
+            public Handler(AppDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var storage = new Storage
+                {
+                    Name = request.Storage.Name,
+                    City = request.Storage.City,
+                    Region = request.Storage.Region,
+                    IsWorking = true,
+                };
+
+
+                _context.Storages.Add(storage);
+
+                await _context.SaveChangesAsync();
+
+                return Unit.Value;
+            }
+        }
+    }
+}
