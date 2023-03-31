8e84add6dd1b668b2be47f7e1280761616a753c2
annushka <anna.bondarenko1@nure.ua>
2023-03-31 09:53:05 +0200

    DeleteStorage handler added

---
+++ Application/Storages/DeleteStorage.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Storages
+{
+    public class DeleteStorage
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly AppDbContext _context;
+
+            public Handler(AppDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var storage = await _context.Storages.FindAsync(request.Id);
+                _context.Storages.Remove(storage);
+
+                await _context.SaveChangesAsync();
+                return Unit.Value;
+            }
+        }
+    }
+}
