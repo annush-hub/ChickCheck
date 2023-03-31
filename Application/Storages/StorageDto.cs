b6e746b7b924bf2a8dda63eab535854b2adb3f3b
annushka <anna.bondarenko1@nure.ua>
2023-03-31 09:52:04 +0200

    StorageDto added

---
+++ Application/Storages/StorageDto.cs
@@ -0,0 +1,18 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Storages
+{
+    public class StorageDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public bool IsWorking { get; set; }
+    }
+}
