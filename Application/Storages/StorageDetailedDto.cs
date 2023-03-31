73de1371cb4caa1d396ec7483cf0f29eab0e0bc8
annushka <anna.bondarenko1@nure.ua>
2023-03-31 09:51:01 +0200

    StoradeDetailedDto added

---
+++ Application/Storages/StorageDetailedDto.cs
@@ -0,0 +1,20 @@
+﻿using Application.EggGrades;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Storages
+{
+    public class StorageDetailedDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public bool IsWorking { get; set; }
+        public ICollection<EggGradeShortDto> EggGrades { get; set; }
+    }
+}
