498159243e69ed96cf0836f2a7c4cb7293bf9c05
annushka <anna.bondarenko1@nure.ua>
2023-03-31 09:52:45 +0200

    StorageList handler added

---
+++ Application/Storages/StorageList.cs
@@ -0,0 +1,40 @@
+﻿using Application.EggGrades;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Storages
+{
+    public class StorageList
+    {
+        public class Query : IRequest<List<StorageDetailedDto>> { }
+
+        public class Handler : IRequestHandler<Query, List<StorageDetailedDto>>
+        {
+            private readonly AppDbContext _context;
+            private readonly IMapper _mapper;
+
+            public Handler(AppDbContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+
+            public async Task<List<StorageDetailedDto>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var storages = await _context.Storages
+                    .ProjectTo<StorageDetailedDto>(_mapper.ConfigurationProvider)
+                    .ToListAsync(cancellationToken: cancellationToken);
+
+                return storages;
+            }
+        }
+    }
+}
