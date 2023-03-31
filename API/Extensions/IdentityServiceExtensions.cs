83fb419b8ad99daf9492f155087f0c0385749cf0
annushka <anna.bondarenko1@nure.ua>
2023-03-31 13:56:37 +0200

    Configuring Identity in program.cs done

---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using Persistence;
+using System.Text;
+
+namespace API.Extensions
+{
+    public static class IdentityServiceExtensions
+    {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services,
+            IConfiguration config)
+        {
+            services.AddIdentity<AppUser, IdentityRole>(opt =>
+            {
+                opt.Password.RequireNonAlphanumeric = false;
+            })
+               .AddEntityFrameworkStores<AppDbContext>();
+
+
+
+            services.AddAuthentication();
+
+            return services;
+        }
+    }
+}
