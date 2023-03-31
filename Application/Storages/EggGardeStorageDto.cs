340630128e8732d43cdc998b3d8950279a5d5daf
annushka <anna.bondarenko1@nure.ua>
2023-03-31 11:16:58 +0200

    EggGradeStorageDto added

---
+++ Application/Storages/EggGardeStorageDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Storages
+{
+    public class EggGardeStorageDto
+    {
+        public Guid EggGradeId { get; set; }
+        //public Guid StorageId { get; set; }
+    }
+}
