4567bf5babb2d2b11c604a6fe4a7c9221d06470c
annushka <anna.bondarenko1@nure.ua>
2023-03-31 19:51:59 +0200

    UserAuthDto added

---
+++ API/Dtos/UserAuthDto.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Dtos
+{
+    public class UserAuthDto
+    {
+        public string DisplayName { get; set; }
+        public string Token { get; set; }
+        public string Username { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
