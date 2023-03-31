c738a7cebcb26ac6698b6340e44765b8d9655843
annushka <anna.bondarenko1@nure.ua>
2023-03-31 08:16:15 +0200

    Storage class added

---
+++ Domain/Storage.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Storage : BaseEntity
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public int IsWorking { get; set; }
+        public ICollection<EggGradeStorage> EggGrades { get; set; } = new List<EggGradeStorage>();
+    }
+}
