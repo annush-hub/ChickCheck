08d360203892a53b4cb6a8949f033130e097f009
annushka <anna.bondarenko1@nure.ua>
2023-03-31 08:16:54 +0200

    EggGradeStorage class added

---
+++ Domain/EggGradeStorage.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class EggGradeStorage
+    {
+        public Guid EggGradeId { get; set; }
+        public EggGrade EggGrade { get; set; }
+        public Guid StorageId { get; set; }
+        public Storage Storage { get; set; }
+    }
+}
