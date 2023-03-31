e8d969635a89de53989d45007eed5fafe687c9eb
annushka <anna.bondarenko1@nure.ua>
2023-03-31 14:33:03 +0200

    Identity Dtos added

---
+++ API/Dtos/RegisterDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Dtos
+{
+    public class RegisterDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string DisplayName { get; set; }
+        public string Username { get; set; }
+    }
+}
