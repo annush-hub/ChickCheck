67dc721ad115c5289cddf7e34682dfe2f3cc1cbf
annushka <anna.bondarenko1@nure.ua>
2023-03-31 09:50:02 +0200

    EggGradeShortDto added

---
+++ Application/EggGrades/EggGradeShortDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.EggGrades
+{
+    public class EggGradeShortDto
+    {
+        public Guid Id { get; set; }
+        public string GradeUA { get; set; }
+        public string GradeEU { get; set; }
+    }
+}
