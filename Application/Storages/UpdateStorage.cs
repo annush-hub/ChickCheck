da687d0173f8b72b446be5f38ebe128dfc5a99a8
annushka <anna.bondarenko1@nure.ua>
2023-03-31 09:52:56 +0200

    UpdateStorage handler added

---
+++ Application/Storages/UpdateStorage.cs
@@ -0,0 +1,44 @@
+﻿using Application.Feeders;
+using AutoMapper;
+using MediatR;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Storages
+{
+    public class UpdateStorage
+    {
+        public class Command : IRequest
+        {
+            public StorageDto Storage { get; set; }
+        }
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly AppDbContext _context;
+            private readonly IMapper _mapper;
+
+
+            public Handler(AppDbContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+
+                var storage = await _context.Storages.FindAsync(request.Storage.Id);
+
+                _mapper.Map(request.Storage, storage);
+
+                await _context.SaveChangesAsync();
+
+                return Unit.Value;
+            }
+        }
+    }
+}
