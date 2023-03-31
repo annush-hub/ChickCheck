f1c9a777a82079a6c771b85db0b01c62d9e5c28f
annushka <anna.bondarenko1@nure.ua>
2023-03-31 09:53:27 +0200

    StoragesController updated

---
+++ API/Controllers/StoragesController.cs
@@ -0,0 +1,46 @@
+﻿using Application.Barns;
+using Application.EggGrades;
+using Application.Feeders;
+using Application.Storages;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StoragesController : BaseApiController
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<StorageDetailedDto>>> GetStorages()
+        {
+            return await Mediator.Send(new StorageList.Query());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<StorageDetailedDto>> GetStorage(Guid id)
+        {
+            return await Mediator.Send(new StorageDetails.Query { Id = id });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateStorage([FromBody] StorageDto storage)
+        {
+            return Ok(await Mediator.Send(new CreateStorage.Command { Storage = storage }));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateStorage(Guid id, [FromBody] StorageDto storage)
+        {
+            storage.Id = id;
+            return Ok(await Mediator.Send(new UpdateStorage.Command { Storage = storage }));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStorage(Guid id)
+        {
+            return Ok(await Mediator.Send(new DeleteStorage.Command { Id = id }));
+        }
+    }
+}
