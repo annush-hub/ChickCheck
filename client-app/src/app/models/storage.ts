f054b000b249a0fab62ef9ad1154505b8bd46bd4
annushka <anna.bondarenko1@nure.ua>
2023-05-25 10:54:33 +0200

    Storages display isWorking and eggGrades

--- client-app/src/app/models/storage.ts
+++ client-app/src/app/models/storage.ts
@@ -1,7 +1,10 @@
+import { EggGrade } from "./eggGrade";
+
 export interface EggStorage {
   id: string;
   name: string;
   city: string;
   region: string;
   isWorking: boolean;
+  eggGrades: EggGrade[];
 }
--- client-app/src/features/storages/dashboard/StorageListItem.tsx
+++ client-app/src/features/storages/dashboard/StorageListItem.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Item, Segment } from "semantic-ui-react";
+import { Item, Label, Segment } from "semantic-ui-react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { faEgg, faLocationDot } from "@fortawesome/free-solid-svg-icons";
 import { EggStorage } from "../../../app/models/storage";
 
 interface Props {
@@ -9,9 +9,17 @@ interface Props {
 }
 
 export default function StorageListItem({ storage }: Props) {
+  const getStatusColor = () => {
+    return storage.isWorking ? "green" : "red";
+  };
+
   return (
     <Segment.Group>
       <Segment>
+        <Label color={getStatusColor()}>
+          {storage.isWorking ? "Active" : "Inactive"}
+        </Label>
+
         <Item.Group>
           <Item>
             <Item.Content>
@@ -22,9 +30,20 @@ export default function StorageListItem({ storage }: Props) {
       </Segment>
       <Segment secondary>
         <span>
-          <FontAwesomeIcon icon={faLocationDot} />
+          <FontAwesomeIcon
+            icon={faLocationDot}
+            style={{ fontSize: "1.3em", color: "#68C6B8" }}
+          />{" "}
           {storage.city}, {storage.region}
         </span>
+      </Segment>
+      <Segment>
+        {storage.eggGrades.map((eggGrade) => (
+          <Label key={eggGrade.id}>
+            {" "}
+            <FontAwesomeIcon icon={faEgg} /> {eggGrade.gradeUA}
+          </Label>
+        ))}
       </Segment>
     </Segment.Group>
   );
