82deb6ad87f554c645ed4b2ec02f626ddb0414f3
annushka <anna.bondarenko1@nure.ua>
2023-05-25 12:06:22 +0200

    i18n added to HomePage

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Container, Header, Segment, Image, Button } from "semantic-ui-react";
+import LanguageSelector from "../LanguageSelector";
+import { useTranslation } from "react-i18next";
 
 export default function HomePage() {
+  const { t } = useTranslation();
   return (
     <Segment inverted textAlign="center" vertical className="masthead">
       <Container text>
@@ -10,18 +13,14 @@ export default function HomePage() {
           <Image src="/assets/logo.png" alt="logo" size="massive" />
           ChickCheck
         </Header>
-        <Header as="h2" content="Welcome" inverted />
+        <Header as="h2" content={t("homePage.welcome")} inverted />
         <Button as={Link} to="/barns" size="huge" inverted>
           Go to Barns
         </Button>
       </Container>
+      <Container>
+        <LanguageSelector />
+      </Container>
     </Segment>
-    // <Container style={{ marginTop: "7em" }}>
-    //   <h1>Home page</h1>
-    //   <h3>
-    //     {" "}
-    //     Go to <Link to="/barns">Barns</Link>
-    //   </h3>
-    // </Container>
   );
 }
