e599a43dd7739c78604611342a230727e808467e
annushka <anna.bondarenko1@nure.ua>
2023-05-27 00:21:38 +0200

    StorageDashboard loading component added

--- client-app/src/features/storages/dashboard/StorageDashboard.tsx
+++ client-app/src/features/storages/dashboard/StorageDashboard.tsx
@@ -3,10 +3,13 @@ import { Grid, GridColumn } from "semantic-ui-react";
 import { observer } from "mobx-react-lite";
 import StorageList from "./StorageList";
 import { useStore } from "../../../app/stores/store";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
+import { useTranslation } from "react-i18next";
 
 export default observer(function StorageDashboard() {
   const { storageStore } = useStore();
-  const { storageList, loadStorages, storageRegistry } = storageStore;
+  const { loadStorages, storageRegistry } = storageStore;
+  const { t } = useTranslation();
 
   useEffect(() => {
     if (storageRegistry.size <= 1) loadStorages();
@@ -15,6 +18,9 @@ export default observer(function StorageDashboard() {
   useEffect(() => {
     storageStore.loadStorages();
   }, [storageStore]);
+
+  if (storageStore.loadingiInitial)
+    return <LoadingComponent content={t("loadingComponent.loadingStorages")} />;
 
   return (
     <Grid>
