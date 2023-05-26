43e254f95466b10045080506e5c2b739400d1131
annushka <anna.bondarenko1@nure.ua>
2023-05-26 21:55:10 +0200

    userStore created

--- client-app/src/app/stores/store.ts
+++ client-app/src/app/stores/store.ts
@@ -3,12 +3,14 @@ import BarnStore from "./barnStore";
 import EggGradeStore from "./eggGradeStore";
 import CommonStore from "./commonStore";
 import StorageStore from "./storageStore";
+import UserStore from "./userStore";
 
 interface Store {
   barnStore: BarnStore;
   eggGradeStore: EggGradeStore;
   storageStore: StorageStore;
   commonStore: CommonStore;
+  userStore: UserStore;
 }
 
 export const store: Store = {
@@ -16,6 +18,7 @@ export const store: Store = {
   eggGradeStore: new EggGradeStore(),
   storageStore: new StorageStore(),
   commonStore: new CommonStore(),
+  userStore: new UserStore(),
 };
 
 export const StoreContext = createContext(store);
